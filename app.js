092f9adab92d90fa752e4fe482a2dedbd86cfadc
Humza920 <humza.rehman.779@gmail.com>
2024-04-25 12:35:47 -0700

    ALMOST DONE

--- app.js
+++ app.js
@@ -3,40 +3,45 @@ const minute = document.querySelector(".mins")
 const hour = document.querySelector(".hour")
 
 function start () {
-let sec = 0
-let mins = 0
-let hours = 0 
-const sixTy = 60
-const twoFour = 24
+    let sec = 0
+    let mins = 0
+    let hours = 0 
+    const sixTy = 60
+    const twoFour = 24
+    
+ const me = setInterval(function () {
+    sec += 1
+    second.innerHTML = sec
+    if (sec === sixTy) {
+          sec = 0 
 
+    } 
+} , 1000 )
+    
 setInterval(function () {
-        sec += 1
-        second.innerHTML = sec
-        if (sec === sixTy) {
-              sec = 0 
+    mins += 1
+    minute.innerHTML = mins
+    if (mins === sixTy) {
+        mins = 0  
 
-        } 
-    } , 1000 )
+  } 
+} , 60000)
+    
+ setInterval(function () {
+    hours += 1
+    hour.innerHTML = hours
+    if (hours === twoFour) {
+        hours = 0  
 
-    setInterval(function () {
-        mins += 1
-        minute.innerHTML = mins
-        if (mins === sixTy) {
-            mins = 0  
+  } 
+} , 6000000)
+    }
 
-      } 
-    } , 60000)
-
-    setInterval(function () {
-        hours += 1
-        hour.innerHTML = hours
-        if (hours === twoFour) {
-            hours = 0  
-
-      } 
-    } , 360000)
+function pause ()  {
+    clearInterval(me)
 }
 
-function pause () {
-    clearInterval(second,minute,hour)
+function reset () {
+
+    location.reload()
 }
