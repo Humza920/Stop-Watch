88f860d6264fcd0336c7468f862d25d8ef7a3da0
Humza920 <humza.rehman.779@gmail.com>
2024-04-25 11:19:09 -0700

    CREATEd

---
+++ app.js
@@ -0,0 +1,42 @@
+const second = document.querySelector(".sec")
+const minute = document.querySelector(".mins")
+const hour = document.querySelector(".hour")
+
+function start () {
+let sec = 0
+let mins = 0
+let hours = 0 
+const sixTy = 60
+const twoFour = 24
+
+setInterval(function () {
+        sec += 1
+        second.innerHTML = sec
+        if (sec === sixTy) {
+              sec = 0 
+
+        } 
+    } , 1000 )
+
+    setInterval(function () {
+        mins += 1
+        minute.innerHTML = mins
+        if (mins === sixTy) {
+            mins = 0  
+
+      } 
+    } , 60000)
+
+    setInterval(function () {
+        hours += 1
+        hour.innerHTML = hours
+        if (hours === twoFour) {
+            hours = 0  
+
+      } 
+    } , 360000)
+}
+
+function pause () {
+    clearInterval(second,minute,hour)
+}
