716ee9d5cc7f40f668edfedd402a6395d0238b85
Humza920 <humza.rehman.779@gmail.com>
2024-04-26 11:22:04 -0700

    DONE

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
+
 const second = document.querySelector(".sec")
 const minute = document.querySelector(".mins")
 const hour = document.querySelector(".hour")
 const but = document.querySelector("but")
-
-// let mins = 0
-// let hours = 0 
-// let sec = 0
-
+let me  ;
+let you ;
+let we ; 
 function start () {
 let sec = 0
 let mins = 0
@@ -14,27 +13,33 @@ let hours = 0
 const sixTy = 60
 const twoFour = 24
 
-const me = setInterval(function () {
+me = setInterval(function () {
 sec += 1
 second.innerHTML = sec
 if (sec === sixTy) {
       sec = 0 } 
 } , 1000 )
 
-const you = setInterval(function () {
+you = setInterval(function () {
 mins += 1
 minute.innerHTML = mins
 if (mins === sixTy) {
     mins = 0  } 
 } , 60000)
 
-const we = setInterval(function () {
+we = setInterval(function () {
 hours += 1
 hour.innerHTML = hours
 if (hours === twoFour) {
     hours = 0 } 
 } , 6000000)
+}
 
+
+
+
+function reset () {
+    location.reload()
 }
 
 
@@ -43,6 +48,4 @@ function pause () {
 }
 
 
-function reset () {
-    location.reload()
-}
+
